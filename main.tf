7707e75c52c1d578066ffad352e7c8fb10aaf1a9
Cibi Chakravarthi <103481860+BlueRush07@users.noreply.github.com>
2024-05-15 14:37:59 +0530

    variables added

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 provider "aws" {
-  region = "us-east-1"
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 resource "aws_route53_record" "a_record" {
-  # zone_id = aws_route53_zone.dns.zone_id
   zone_id = var.aws_zoneid
   name    = var.sub_domain_name
   type    = "A"
--- variables.tf
+++ variables.tf
@@ -12,3 +12,15 @@ variable "aws_zoneid" {
   description = "Enter the zone id of the route53 hosing"
   type        = string
 }
+variable "aws_access_key" {
+  description = "Enter the aws_access_key to access route53 hosing"
+  type = string
+}
+variable "aws_secret_key" {
+  description = "Enter the aws_secret_key to access route53 hosing"
+  type = string
+}
+variable "aws_region" {
+  description = "Enter the region where the route53 is hosted"
+  type = string
+}
